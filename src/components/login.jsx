416fc33e9613481d0011201a4e2fe6e7280758d0
MatheusZuchiBalbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-03-21 09:51:50 -0300

    Add files via upload

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import styles from './login.module.css';
 
 function Login() {
+
+    const [login, setLogin] = useState(['']);
+    const [password, setPassword] = useState(['']);
+
+    const CheckLogin = () => {
+        if(login === 'Matheus' && password === '123') {
+            console.log('a');
+            localStorage.setItem('validation', 'true')
+            const validation = localStorage.getItem('validation')
+            console.log(validation)
+        }
+    }
+
     return (
-        <div className="register_div">
-            <form className="login_form">
-                <h3>Formulário de Login</h3>
-                <div className="user_div">
-                    <input type="text" className="user_input" placeholder="Digite o seu usuário: "></input>
+        <div className={styles.login_div}>
+            <form className={styles.login_form}>
+                <h3 className={styles.form_title}>Formulário de Login</h3>
+                <div className={styles.user_div}>
+                    <label htmlFor="user_input" style={{margin: 'auto 0'}}> Usuário: </label>
+                    <input type="text" className={styles.user_password_input} placeholder="Digite o seu usuário: " onChange={(e) => {setLogin(e.target.value)}}></input>
                 </div>
-                <div className="password_div">
-                    <input type="password" className="user_input" placeholder="Digite a sua senha: "></input>
+                <div className={styles.password_div}>
+                    <label htmlFor="user_input" style={{margin: 'auto 0'}}> Password: </label>
+                    <input type="password" className={styles.user_password_input} placeholder="Digite a sua senha: " onChange={(e) => {setPassword(e.target.value)}}></input>
                 </div>
-                <button type="button" onClick={console.log('enviou')}> Enviar </button>
-                <button type="button" onClick={console.log('enviou')}> Não possui uma conta? </button>
+                <div className={styles.buttons_div}>
+                    <button type="button" className={styles.button_send} onClick={CheckLogin}> Enviar </button>
+                    <button type="button" className={styles.button_forgot_password}> Não possui uma conta? </button>
+                </div>
             </form>
         </div>
     )
