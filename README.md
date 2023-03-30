8aef4a301452c29f33126d6939eed62683a95c52
MatheusZuchiBalbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-03-29 21:12:00 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-## CreateBurger
-
-Site criado com o objetivo de melhorar as minhas habilidades de conexão do front-end com o back-end.
-Para essa aplicação expandi os meus conhecimentos react e aprendi/apliquei conhecimentos em Nodejs.
+# CreateBurger
--- src/pages/CreateBurguer.jsx
+++ src/pages/CreateBurguer.jsx
@@ -1,24 +1,54 @@
+import {BsArrowDown} from "react-icons/bs";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 export default function CreateBurguer () {
     return (
+        <>
+        <Header />
         <div className="CreateBurguer_div">
             <div className="Chooses">
                 <h3>Escolha os ingredientes do seu Hambúrguer: </h3>
                 <div className="Bread">
                     <p> Escolha o Pão: </p>
+                    <input type="text" className="dropdown_input"></input> <BsArrowDown />
+                    <div className="bread_options">
+                        <p>ABC</p>
+                        <p>DEF</p>
+                        <p>GHI</p>
+                    </div>
                 </div>
                 <div className="Meat">
                     <p> Escolha a Carne: </p>
+                    <input type="text" className="dropdown_input"></input> <BsArrowDown />
+                    <div className="meat_options">
+                        
+                    </div>
                 </div>
                 <div className="Sauce">
                     <p> Escolha o Tempero: </p>
+                    <input type="text" className="dropdown_input"></input> <BsArrowDown />
+                    <div className="sauce_options">
+                        
+                    </div>
                 </div>
                 <div className="Salads">
                     <p> Escolha as Saladas: </p>
+                    <input type="text" className="dropdown_input"></input> <BsArrowDown />
+                    <div className="salads_options">
+                        
+                    </div>
                 </div>
                 <div className="Cheese">
                     <p> Escolha o Queijo: </p>
+                    <input type="text" className="dropdown_input"></input> <BsArrowDown />
+                    <div className="cheese_options">
+                        
+                    </div>
                 </div>
             </div>
         </div>
+        <Footer />
+        </>
     )
 }
