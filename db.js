721ec836e366d62c3ffa3568f657076a079d450c
Matheus Zuchi Balbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-04-11 11:11:27 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,44 @@
+import express from "express"
+import mysql from "mysql"
+
+
+const app_express = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "createb"
+
+})
+
+app_express.use(express.json())
+
+app_express.get("/", (req, res) => {
+    res.json("Hello This is the Backend")
+});
+
+app_express.get ("/options", (req, res) => {
+    const q = "SELECT * FROM options"
+    db.query(q,(error,data)=> {
+        if(error) {
+            return res.json("A consulta deu o seguinte erro: "+error)
+        }
+        return res.json(data)
+    })
+});
+
+app_express.post("/options", (req, res) => {
+    const q = "INSERT INTO options(`bread`,`meat`,`meat_state`,`salads`,`cheese`) VALUES (?)"
+    const values = [req.body.bread,req.body.meat,req.body.meat_state,req.body.salads,req.body.cheese]
+    db.query(q, [values], (error,data) => {
+        if(error) {
+            return res.json("A inserção de elementos deu o seguinte erro: "+error)
+        }
+        return res.json(data)
+    })
+})
+
+app_express.listen(8800, ()=> {
+    console.log("Coneccted with backend!!")
+} )
