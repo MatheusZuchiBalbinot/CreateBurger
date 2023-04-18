0604902a485428bdb97ea29294a02b217f22ac15
Matheus Zuchi Balbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-04-18 10:29:53 -0300

    Add files via upload

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,42 +7,51 @@ import ContentCards from "./ContentCards";
 export default function Content() {
 
     const house_ready_orders_itens = [{
-        um: {
+        order_1: {
+            name: "La fish",
             bread: "Brioche",
             meat: "Peixe",
             meat_state: "Ao ponto",
             salads: "Tomate e Alface",
             cheese: "Cheddar",
-            image: "https://www.receitasdemae.com.br/wp-content/uploads/2017/12/Hamb%C3%BArguer-f%C3%A1cil-de-peixe.jpg"
+            image: "https://www.receitasdemae.com.br/wp-content/uploads/2017/12/Hamb%C3%BArguer-f%C3%A1cil-de-peixe.jpg",
+            price: 29.99,
         },
-        dois: {
+        order_2: {
+            name: "Suíno do Pecado",
             bread: "Pão italiano",
             meat: "Porco",
             meat_state: "Ao ponto",
             salads: "Tomate e Pepino",
             cheese: "Mussarela",
-            image: "https://acarnequeomundoprefere.com.br/uploads/media/image/frimesa-receita-hamburguer-suino_smlr.jpg"
+            image: "https://acarnequeomundoprefere.com.br/uploads/media/image/frimesa-receita-hamburguer-suino_smlr.jpg",
+            price: 30.99,
         },
-        três: {
+        order_3: {
+            name: "Boi Angelical",
             bread: "Pão de azeite",
             meat: "Boi",
-            meat_state: "Ao ponto para mal",
+            meat_state: "Ao ponto",
             salads: "Alface",
             cheese: "Queijo de Cabra",
-            image: "https://www.pingodoce.pt/wp-content/uploads/2015/05/pd_4660.jpg"
+            image: "https://img.freepik.com/fotos-premium/hamburguer-classico-pao-caseiro-queijo-legumes-costeleta-grelhada-carne-de-porco-carne-de-porco-boi-ou-vitela_88242-15584.jpg?w=2000",
+            price: 33.99,
         },
-        quatro: {
-            bread: "Pão autraliano",
+        order_4: {
+            name: "Frango Voador",
+            bread: "Frango",
             meat: "Frango",
             meat_state: "Ao ponto",
-            salads: "Tomate, Alface e Cebola",
+            salads: "Tomate e Alface",
             cheese: "Emmental",
-            image: "https://www.sabornamesa.com.br/media/k2/items/cache/b0ccfdc71ad7b1e5260a567a30b9ecab_XL.jpg"
+            image: "https://cdn.minhareceita.com.br/2021/01/shutterstock_574607542-scaled.jpg",
+            price: 27.99,
         },
         
     }]
 
     console.log(house_ready_orders_itens)
+    // console.log(house_ready_orders_itens[0].order_1)
 
     const navigate = useNavigate();
 
@@ -58,10 +67,10 @@ export default function Content() {
             <h3 className={styles.house_orders_title}> Hambúrgueres da Casa: </h3>
 
             <div className={styles.house_ready_orders}>
-                <ContentCards />
-                <ContentCards />
-                <ContentCards />
-                <ContentCards />
+                <ContentCards name={house_ready_orders_itens[0].order_1.name} image={house_ready_orders_itens[0].order_1.image} price={house_ready_orders_itens[0].order_1.price} bread={house_ready_orders_itens[0].order_1.bread} meat={house_ready_orders_itens[0].order_1.meat} meat_state={house_ready_orders_itens[0].order_1.meat_state} salads={house_ready_orders_itens[0].order_1.salads} cheese={house_ready_orders_itens[0].order_1.cheese}/>
+                <ContentCards name={house_ready_orders_itens[0].order_2.name} image={house_ready_orders_itens[0].order_2.image} price={house_ready_orders_itens[0].order_2.price} bread={house_ready_orders_itens[0].order_2.bread} meat={house_ready_orders_itens[0].order_2.meat} meat_state={house_ready_orders_itens[0].order_2.meat_state} salads={house_ready_orders_itens[0].order_2.salads} cheese={house_ready_orders_itens[0].order_2.cheese}/>
+                <ContentCards name={house_ready_orders_itens[0].order_3.name} image={house_ready_orders_itens[0].order_3.image} price={house_ready_orders_itens[0].order_3.price} bread={house_ready_orders_itens[0].order_3.bread} meat={house_ready_orders_itens[0].order_3.meat} meat_state={house_ready_orders_itens[0].order_3.meat_state} salads={house_ready_orders_itens[0].order_3.salads} cheese={house_ready_orders_itens[0].order_3.cheese}/>
+                <ContentCards name={house_ready_orders_itens[0].order_4.name} image={house_ready_orders_itens[0].order_4.image} price={house_ready_orders_itens[0].order_4.price} bread={house_ready_orders_itens[0].order_4.bread} meat={house_ready_orders_itens[0].order_4.meat} meat_state={house_ready_orders_itens[0].order_4.meat_state} salads={house_ready_orders_itens[0].order_4.salads} cheese={house_ready_orders_itens[0].order_4.cheese}/>
             </div>
             <div className={styles.characteristics}>
                     <div classname={styles.delivery}>
--- src/components/ContentCards.jsx
+++ src/components/ContentCards.jsx
@@ -1,21 +1,21 @@
 import styles from "./modules/ContentCards.module.css"
 
-export default function ContentCards ({name, image, ingredients, price}) {
+export default function ContentCards ({name, image, price, bread, meat, meat_state, salads, cheese}) {
 
 
     return (
         <div className={styles.card}>
-            <h3>Nome: </h3>
             <div class="card">
-                <img src="https://www.receitasdemae.com.br/wp-content/uploads/2017/12/Hamb%C3%BArguer-f%C3%A1cil-de-peixe.jpg" className={styles.card_image}></img>
-                <div className="ingredients">
-                    <p className={styles.card_p}>Pão: </p>
-                    <p className={styles.card_p}>Carne: </p>
-                    <p className={styles.card_p}>Ponto da Carne: </p>
-                    <p className={styles.card_p}>Saladas: </p>
-                    <p className={styles.card_p}>Queijo: </p>
+                <img src={image} className={styles.card_image}></img>
+                <h3 className={styles.card_title}>{name} </h3>
+                <div className={styles.ingredients}>
+                    <p className={styles.card_p}>Pão: {bread}</p>
+                    <p className={styles.card_p}>Carne: {meat}</p>
+                    <p className={styles.card_p}>Ponto da Carne: {meat_state}</p>
+                    <p className={styles.card_p}>Saladas: {salads}</p>
+                    <p className={styles.card_p}>Queijo: {cheese}</p>
                 </div>
-                <h3 className={styles.card_price}>Preço: </h3>
+                <h3 className={styles.card_price}>Preço: R${price}</h3>
             </div>
         </div>
     )
