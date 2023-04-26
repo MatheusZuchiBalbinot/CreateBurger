de4e0106c30278468921a590e6f3136974fd8161
MatheusZuchiBalbinot <matheusbalbinotzuchi@gmail.com>
2023-04-25 21:48:53 -0300

    Revert "Conexão do Banco com o Login e apresentação de Usuário"
    This reverts commit 3fdeceb40d8ac4736474bf931ccdf01f09761d96.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,75 +1,45 @@
-import { useState, useEffect } from "react";
-import axios from 'axios'
+import { useState } from "react";
 import styles from './modules/Login.module.css';
-import {BsFillPersonFill, BsFillLockFill} from 'react-icons/bs';
-import { useNavigate } from "react-router-dom";
+import {BsFillPersonFill, BsFillLockFill} from 'react-icons/bs'
 
 function Login() {
 
-    const [login, setLogin] = useState({});
-
-    const [username, setUsername] = useState(['']);
+    const [login, setLogin] = useState(['']);
     const [password, setPassword] = useState(['']);
 
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchPassword = async () => {
-            try {
-                const fetch_login = await axios.get("http://localhost:8800/login")
-                setLogin(fetch_login.data)
-            } 
-            catch (err) {
-                console.log(err) 
-            }
-        }
-        fetchPassword()
-    }, [])
-
-    function trade_page() {
-        return navigate("/home");
-    }
-
-    // function RegisterPage() {
-    //     return navigate("/register");
-    // }
-
     const CheckLogin = () => {
-
-        console.log(login)
-        for(var i = 0; i < Object.keys(login).length; i++) {
-            if(username === login[i].username && password === login[i].password) {
-                localStorage.setItem("logged_username", login[i].username)
-                return trade_page();
-            }
-            else {
-                var form_field = document.getElementById("form_field")
-                return form_field.style.border = "3px solid red" 
-            }
+        if(login === 'asd' && password === '123') {
+            return window.location.href="/home";
+            // localStorage.setItem('validation', 'true')
+            // const validation = localStorage.getItem('validation')
+            // console.log(validation)
+        }
+        else {
+            return <alert> A senha de acesso está errada </alert>
         }
     }
 
     return (
         <div className={styles.login_div}>
-            <form className={styles.login_form} id="form_field">
+            <form className={styles.login_form}>
                 <h3 className={styles.form_title}>Formulário de Login</h3>
                 <div className={styles.user_div}>
                     <label htmlFor="user_input" className={styles.titles_style}> Usuário: </label>
                     <div className={styles.input_with_icon}>
                         <BsFillPersonFill />
-                        <input type="text" id="username_input" className={styles.user_password_input} placeholder="Digite o seu usuário: " onChange={(e) => {setUsername(e.target.value)}}></input>
+                        <input type="text" className={styles.user_password_input} placeholder="Digite o seu usuário: " onChange={(e) => {setLogin(e.target.value)}}></input>
                     </div>
                 </div>
                 <div className={styles.password_div}>
                     <label htmlFor="user_input" className={styles.titles_style}> Password: </label>
                     <div className={styles.input_with_icon}>
                         <BsFillLockFill />
-                        <input type="password" id="password_input" className={styles.user_password_input} placeholder="Digite a sua senha: " onChange={(e) => {setPassword(e.target.value)}}></input>
+                        <input type="password" className={styles.user_password_input} placeholder="Digite a sua senha: " onChange={(e) => {setPassword(e.target.value)}}></input>
                     </div>
                 </div>
                 <div className={styles.buttons_div}>
                     <button type="button" className={styles.button_buttons} onClick={CheckLogin}> Enviar </button>
-                    <button type="button" className={styles.button_buttons} > Não possui uma conta? </button>
+                    <button type="button" className={styles.button_buttons}> Não possui uma conta? </button>
                 </div>
             </form>
             <div className={styles.gif_image}>
